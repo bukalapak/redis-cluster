376e33e30a71c97934bd44d72bc8d22321011bfc
David Christopher <roronoalance@gmail.com>
2019-06-28 16:16:57 +0700

    fix rubocop

--- lib/redis_cluster/circuit.rb
+++ lib/redis_cluster/circuit.rb
@@ -5,29 +5,23 @@ class RedisCluster
   # Circuit is circuit breaker for RedisCluster.
   class Circuit
 
-    attr_reader :fail_count, :ban_until  
+    attr_reader :fail_count, :ban_until
 
     DEFAULT_FAIL_THRESHOLD  = 5
     DEFAULT_INTERVAL_SECOND = 5
 
     def initialize
       @ban_until = Time.now
-      @fail_count = 0 
+      @fail_count = 0
       @fail_threshold = DEFAULT_FAIL_THRESHOLD
       @last_fail_time = Time.now
       @interval_time = DEFAULT_INTERVAL_SECOND
     end
 
     def failed
-      if (@last_fail_time + @interval_time).utc < Time.now
-        @fail_count = 0
-      end
-
+      @fail_count = 0 if (@last_fail_time + @interval_time).utc < Time.now
       @fail_count += 1
-
-      if check_threshold
-        open!
-      end
+      open! if check_threshold
     end
 
     def open!
@@ -35,14 +29,11 @@ class RedisCluster
     end
 
     def open?
-      if @ban_until < Time.now
-        return false
-      else
-        return true
-      end
+      return false if @ban_until < Time.now
+      true
     end
 
-    private 
+    private
 
     def check_threshold
       @fail_count >= @fail_threshold
