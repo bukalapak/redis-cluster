9531369b5489b655cccb9ed32cce053a96b2e6c8
David Christopher <roronoalance@gmail.com>
2019-07-01 14:37:58 +0700

    add documentation

--- lib/redis_cluster/circuit.rb
+++ lib/redis_cluster/circuit.rb
@@ -11,10 +11,14 @@ class RedisCluster
       @ban_until = Time.now
       @fail_count = 0
       @last_fail_time = Time.now
-      @fail_threshold = threshold 
-      @interval_time = interval 
+      @fail_threshold = threshold
+      @interval_time = interval
     end
 
+    # Failed is a method to add failed count and compare it to threshold,
+    # Will trip circuit if the count goes through threshold.
+    #
+    # @return[void]
     def failed
       @fail_count = 0 if (@last_fail_time + @interval_time).utc < Time.now
       @fail_count += 1
@@ -22,10 +26,16 @@ class RedisCluster
       open! if @fail_count >= @fail_threshold
     end
 
+    # Open! is a method to update ban time.
+    #
+    # @return[void] 
     def open!
       @ban_until = (Time.now + @interval_time).utc
     end
 
+    # Open? is a method to check if the circuit breaker status.
+    #
+    # @return[Boolean] Wheter the circuit is open or not
     def open?
       return false if @ban_until <= Time.now
       true
