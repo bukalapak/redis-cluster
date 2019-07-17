3e32fd393dfac9d1c38e654e9123ff008a41680a
emon <yusuf.imam@bukalapak.com>
2019-07-17 11:37:46 +0700

    add 'caller:'

--- lib/redis_cluster/client.rb
+++ lib/redis_cluster/client.rb
@@ -78,7 +78,7 @@ class RedisCluster
     private
 
     def _commit
-      raise CircuitOpenError, "Circuit open in client #{url} until #{@circuit.ban_until} #{@circuit.callers.map{ |e| e.join("\n") }.join("\n")}" if @circuit.open?
+      raise CircuitOpenError, "Circuit open in client #{url} until #{@circuit.ban_until} callers: #{@circuit.callers.map{ |e| e.join("\n") }.join("\n")}" if @circuit.open?
       return nil if queue.empty?
 
       result = Array.new(queue.size)
